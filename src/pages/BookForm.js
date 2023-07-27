9c850d1992eefd86f451d6aac1502eca53e8b64e
logan676 <logan676395@gmail.com>
2023-07-27 23:55:29 +0800

    书籍信息

--- src/pages/BookForm.js
+++ src/pages/BookForm.js
@@ -1,202 +1,332 @@
 // BookForm.js
 import React, { useState } from 'react';
-import { TextField, Button, FormControl, InputLabel, Select, MenuItem, Box } from '@mui/material';
+import {
+  TextField,
+  Button,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Box,
+  Divider,
+  Grid,
+  Typography,
+} from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { deDE as coreDeDE } from '@mui/material/locale';
 import { deDE } from '@mui/x-date-pickers/locales';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-// date-fns
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-// or for dayjs
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { v4 as uuidv4 } from 'uuid'; // Import uuidv4 to generate unique IDs
-import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
+import axios from './axiosInstance';
+import UploadImage from './UploadImage';
+import BookList from './BookList';
 
 const BookForm = () => {
   const [bookData, setBookData] = useState({
-    id: uuidv4(), // Generate unique ID for the book
-    authors: '',
-    isbn: '',
-    reviews: '',
-    excerpts: '',
     title: '',
-    purchaseYear: null, // Use null for date pickers to support calendar dates
-    publicationYear: null, // Use null for date pickers to support calendar dates
-    publisherInfo: '',
+    authors: [], // Change to an empty array
+    tags: [], // Change to an empty array
+    topics: [], // Change to an empty array
     readingStatus: '想读',
     readingProgress: '',
-    tags: '',
-    topics: '',
+    publisherInfo: '', // Update this field based on your actual publisher data
+    isbn: '',
+    reviews: [], // Change to an empty array
+    excerpts: [], // Change to an empty array
+    purchaseYear: '',
+    publicationYear: '',
+    pic: null,
   });
+
+  const [error, setError] = useState(null);
+  const [refreshList, setRefreshList] = useState(false);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setBookData((prevData) => ({ ...prevData, [name]: value }));
+    if (name === 'authors' || name === 'tags' || name === 'topics' || name === 'reviews' || name === 'excerpts') {
+      // Split the input value based on commas to create an array
+      setBookData((prevData) => ({ ...prevData, [name]: value.split(',') }));
+    } else {
+      setBookData((prevData) => ({ ...prevData, [name]: value }));
+    }
   };
 
   const handleDateChange = (name, date) => {
     setBookData((prevData) => ({ ...prevData, [name]: date }));
   };
 
-  const handleSubmit = () => {
-    // Send the bookData to your backend or API using axios
-    axios
-      .post('/api/books', bookData)
-      .then((response) => {
-        console.log('Book data submitted successfully:', response.data);
-        // Add any success handling or navigation logic here
-      })
-      .catch((error) => {
-        console.error('Error submitting book data:', error);
-        // Add error handling logic here
+  const handleImageSelect = (file) => {
+    setBookData((prevData) => ({ ...prevData, pic: file }));
+  };
+
+  const convertToArray = (field) => {
+    if (typeof field === 'string') {
+      return field.split(',').map((item) => item.trim());
+    }
+    return Array.isArray(field) ? field.map((item) => item.trim()) : [];
+  };
+
+  // Function to read a file as a base64-encoded data URL
+  const readFileAsDataURL = (file) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = reject;
+      reader.readAsDataURL(file);
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const missingFields = [];
+    if (!bookData.title) missingFields.push('书名');
+    if (bookData.authors.length === 0) missingFields.push('作者 (逗号分割)');
+    if (bookData.tags.length === 0) missingFields.push('标签 (逗号分割)');
+    if (bookData.topics.length === 0) missingFields.push('阅读专题 (逗号分割)');
+    if (!bookData.readingStatus) missingFields.push('Reading Status');
+    if (!bookData.readingProgress) missingFields.push('阅读进度 (%)');
+    if (!bookData.publisherInfo) missingFields.push('出版信息');
+    if (!bookData.isbn) missingFields.push('ISBN');
+    if (bookData.reviews.length === 0) missingFields.push('书评');
+    if (bookData.excerpts.length === 0) missingFields.push('书摘');
+    if (!bookData.purchaseYear) missingFields.push('购买日期');
+    if (!bookData.publicationYear) missingFields.push('出版日期');
+    if (!bookData.pic) missingFields.push('图片');
+
+    if (missingFields.length > 0) {
+      setError(`请填写以下必填字段: ${missingFields.join(', ')}`);
+      return;
+    }
+
+    // const response = await axios.get(`/api/books?name=${bookData.title}`);
+    // if (response.data.length > 0) {
+    //   setError('已经存在同名书.');
+    //   return;
+    // }
+
+    setError(null);
+    try {
+      const authorsArray = convertToArray(bookData.authors);
+      const tagsArray = convertToArray(bookData.tags);
+      const topicsArray = convertToArray(bookData.topics);
+      const reviewsArray = convertToArray(bookData.reviews);
+      const excerptsArray = convertToArray(bookData.excerpts);
+
+      bookData.authors = authorsArray;
+      bookData.tags = tagsArray;
+      bookData.topics = topicsArray;
+      bookData.reviews = reviewsArray;
+      bookData.excerpts = excerptsArray;
+
+      const picFile = bookData.pic; // Assuming bookData.pic is a File object from a file input
+
+      if (picFile) {
+        try {
+          const picData = await readFileAsDataURL(picFile);
+          bookData.pic = picData; // Now pic is converted to a base64-encoded string
+        } catch (error) {
+          console.error('Error converting pic to string:', error);
+          setError('图片转换失败，请重试');
+          return;
+        }
+      }
+
+      const response = await axios.post('/api/books', bookData, {
+        headers: { 'Content-Type': 'multipart/form-data' },
       });
+      console.log('书籍信息添加成功:', response.data);
+      setError(null);
+      setBookData({
+        title: '',
+        authors: [],
+        tags: [],
+        topics: [],
+        readingStatus: '想读',
+        readingProgress: '',
+        publisherInfo: '',
+        isbn: '',
+        reviews: [],
+        excerpts: [],
+        purchaseYear: '',
+        publicationYear: '',
+        pic: null,
+      });
+      setRefreshList(true);
+    } catch (error) {
+      console.error('书籍信息添加失败:', error);
+      setError(`书籍信息提交失败：${error.message}`);
+    }
   };
 
   return (
-    <div>
-      <h1>Enter Book Information</h1>
-      <form>
-        <Box mb={2}>
+    <Box p={3}>
+      <Typography variant="h4">书架管理</Typography>
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={6} sm={3}>
+            <TextField
+              label="书名"
+              name="title"
+              value={bookData.title}
+              onChange={handleInputChange}
+              margin="normal"
+              fullWidth
+              required
+            />
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <TextField
+              label="作者 (逗号分割)"
+              name="authors"
+              value={bookData.authors}
+              onChange={handleInputChange}
+              margin="normal"
+              fullWidth
+              required
+            />
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <TextField
+              label="标签 (逗号分割)"
+              name="tags"
+              margin="normal"
+              value={bookData.tags}
+              onChange={handleInputChange}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <TextField
+              margin="normal"
+              label="阅读专题 (逗号分割)"
+              name="topics"
+              value={bookData.topics}
+              onChange={handleInputChange}
+              fullWidth
+            />
+          </Grid>
+        </Grid>
+        <Grid container spacing={2}>
+          <Grid item xs={6} sm={3}>
+            <FormControl fullWidth required margin="normal">
+              <InputLabel>Reading Status</InputLabel>
+              <Select name="readingStatus" value={bookData.readingStatus} onChange={handleInputChange}>
+                <MenuItem value="已读">已读</MenuItem>
+                <MenuItem value="在阅读">在阅读</MenuItem>
+                <MenuItem value="想读">想读</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <TextField
+              margin="normal"
+              label="阅读进度 (%)"
+              name="readingProgress"
+              type="number"
+              value={bookData.readingProgress}
+              onChange={handleInputChange}
+              fullWidth
+              inputProps={{ min: 0, max: 100 }}
+            />
+          </Grid>
+
+          <Grid item xs={6} sm={3}>
+            <TextField
+              label="出版信息"
+              name="publisherInfo"
+              value={bookData.publisherInfo}
+              onChange={handleInputChange}
+              margin="normal"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <TextField
+              label="ISBN"
+              name="isbn"
+              margin="normal"
+              value={bookData.isbn}
+              onChange={handleInputChange}
+              fullWidth
+              required
+            />
+          </Grid>
+        </Grid>
+        <Grid item xs={6} sm={3}>
           <TextField
-            label="Book Title"
-            name="title"
-            value={bookData.title}
-            onChange={handleInputChange}
-            fullWidth
-            required
-          />
-        </Box>
-        <Box mb={2}>
-          <TextField
-            label="Authors (comma-separated)"
-            name="authors"
-            value={bookData.authors}
-            onChange={handleInputChange}
-            fullWidth
-            required
-          />
-        </Box>
-        <Box mb={2}>
-          <TextField label="ISBN" name="isbn" value={bookData.isbn} onChange={handleInputChange} fullWidth required />
-        </Box>
-        <Box mb={2}>
-          <TextField
-            label="Reviews (comma-separated)"
-            name="reviews"
-            value={bookData.reviews}
-            onChange={handleInputChange}
-            fullWidth
-          />
-        </Box>
-        <Box mb={2}>
-          <TextField
-            label="Excerpts (comma-separated)"
+            label="书摘"
+            margin="normal"
             name="excerpts"
             value={bookData.excerpts}
             onChange={handleInputChange}
             fullWidth
           />
-        </Box>
-        <Box mb={2}>
-          <FormControl fullWidth required>
-            <InputLabel>Reading Status</InputLabel>
-            <Select name="readingStatus" value={bookData.readingStatus} onChange={handleInputChange}>
-              <MenuItem value="已读">已读</MenuItem>
-              <MenuItem value="在阅读">在阅读</MenuItem>
-              <MenuItem value="想读">想读</MenuItem>
-            </Select>
-          </FormControl>
-        </Box>
-        <Box mb={2}>
+        </Grid>
+        <Grid item xs={6} sm={3}>
           <TextField
-            label="Reading Progress (%)"
-            name="readingProgress"
-            type="number"
-            value={bookData.readingProgress}
-            onChange={handleInputChange}
-            fullWidth
-            inputProps={{ min: 0, max: 100 }} // Ensure the input only accepts numeric values between 0 and 100
-          />
-        </Box>
-        <Box mb={2}>
-          <TextField
-            label="Tags (comma-separated)"
-            name="tags"
-            value={bookData.tags}
+            label="书评"
+            margin="normal"
+            name="reviews"
+            value={bookData.reviews}
             onChange={handleInputChange}
             fullWidth
           />
-        </Box>
-        <Box mb={2}>
-          <TextField
-            label="Topics (comma-separated)"
-            name="topics"
-            value={bookData.topics}
-            onChange={handleInputChange}
-            fullWidth
-          />
-        </Box>
-        <Box mb={2}>
-          {/* <DatePicker
-            label="Purchase Year"
-            name="purchaseYear"
-            value={bookData.purchaseYear}
-            onChange={(date) => handleDateChange('purchaseYear', date)}
-            renderInput={(params) => <TextField {...params} />}
-          /> */}
-
-          <LocalizationProvider
-            dateAdapter={AdapterDateFns}
-            localeText={deDE.components.MuiLocalizationProvider.defaultProps.localeText}
-          >
-            <DatePicker
-              label="Purchase Year"
-              name="purchaseYear"
-              views={['year', 'month', 'day']}
-              value={bookData.purchaseYear}
-              onChange={(date) => handleDateChange('purchaseYear', date)}
-              renderInput={(params) => <TextField {...params} />}
-            />
-          </LocalizationProvider>
-        </Box>
-        <Box mb={2}>
-          {/* <DatePicker
-            label="Publication Year"
-            name="publicationYear"
-            value={bookData.publicationYear}
-            onChange={(date) => handleDateChange('publicationYear', date)}
-            renderInput={(params) => <TextField {...params} />}
-          /> */}
-          <LocalizationProvider
-            dateAdapter={AdapterDateFns}
-            localeText={deDE.components.MuiLocalizationProvider.defaultProps.localeText}
-          >
-            <DatePicker
-              label="Publication Year"
-              name="publicationYear"
-              views={['year', 'month', 'day']}
-              defaultValue={new Date(2022, 1, 1)}
-              value={bookData.publicationYear}
-              onChange={(date) => handleDateChange('publicationYear', date)}
-              renderInput={(params) => <TextField {...params} />}
-            />
-          </LocalizationProvider>
-        </Box>
-        <Box mb={2}>
-          <TextField
-            label="Publisher Info"
-            name="publisherInfo"
-            value={bookData.publisherInfo}
-            onChange={handleInputChange}
-            fullWidth
-          />
-        </Box>
-        <Button variant="contained" color="primary" onClick={handleSubmit}>
-          Submit
+        </Grid>
+        <Grid container spacing={2}>
+          <Grid item xs={6} sm={3} mt={3}>
+            <LocalizationProvider
+              dateAdapter={AdapterDateFns}
+              localeText={deDE.components.MuiLocalizationProvider.defaultProps.localeText}
+            >
+              <DatePicker
+                label="购买日期"
+                name="purchaseYear"
+                views={['year', 'month', 'day']}
+                value={bookData.purchaseYear}
+                onChange={(date) => handleDateChange('purchaseYear', date)}
+                renderInput={(params) => <TextField {...params} margin="normal" />}
+              />
+            </LocalizationProvider>
+          </Grid>
+          <Grid item xs={6} sm={3} mt={3}>
+            <LocalizationProvider
+              dateAdapter={AdapterDateFns}
+              localeText={deDE.components.MuiLocalizationProvider.defaultProps.localeText}
+            >
+              <DatePicker
+                label="出版日期"
+                name="publicationYear"
+                views={['year', 'month', 'day']}
+                defaultValue={new Date(2022, 1, 1)}
+                value={bookData.publicationYear}
+                onChange={(date) => handleDateChange('publicationYear', date)}
+                renderInput={(params) => <TextField {...params} margin="normal" />}
+              />
+            </LocalizationProvider>
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <UploadImage onImageSelect={(file) => handleImageSelect(file)} />
+        </Grid>
+        <Button variant="contained" color="primary" type="submit">
+          新增图书信息
         </Button>
+        {error && (
+          <Box mt={2}>
+            <Typography variant="body1" color="error">
+              {error}
+            </Typography>
+          </Box>
+        )}
       </form>
-    </div>
+      <Box my={3}>
+        <Divider />
+        <BookList refresh={refreshList} />
+      </Box>
+    </Box>
   );
 };
 
--- src/pages/BookList.js
+++ src/pages/BookList.js
@@ -1,32 +1,137 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@mui/styles';
+import { Card, CardActionArea, CardContent, CardMedia, Typography, IconButton, Button, Box, Grid } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import axios from './axiosInstance';
 
-const BookList = () => {
+const useStyles = makeStyles((theme) => ({
+  card: {
+    display: 'flex',
+    width: 500,
+    height: 200,
+    padding: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+  },
+  cardContent: {
+    flex: '1 0 auto',
+  },
+  cardMedia: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+    border: '1px solid #ccc',
+    padding: 5,
+  },
+  actionArea: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+}));
+
+const BookList = ({ refresh }) => {
+  const classes = useStyles();
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
     fetchBooks();
-  }, []);
+  }, [refresh]);
 
   const fetchBooks = async () => {
     try {
       const response = await axios.get('/api/books');
       setBooks(response.data);
+      console.log(response.data);
     } catch (error) {
-      console.error(error);
+      console.error('获取书籍信息失败:', error);
     }
   };
 
+  const handleDelete = async (name) => {
+    try {
+      console.log(`handleDelete:${name}`);
+      await axios.delete(`/api/authors/${name}`);
+      console.log(`delete:${name}`);
+      fetchBooks();
+    } catch (error) {
+      console.error('删除书籍信息失败:', error);
+    }
+  };
+
+  const handleEdit = (id) => {
+    console.log('编辑书籍信息ID:', id);
+  };
+
+  const imageHost = 'http://localhost:5555';
+
   return (
     <div>
-      <h1>书籍列表</h1>
-      {books.map((book) => (
-        <div key={book._id}>
-          <h3>{book.title}</h3>
-          <p>作者: {book.author}</p>
-          {/* 其他书籍信息 */}
-        </div>
-      ))}
+      <Typography variant="h5" gutterBottom>
+        书籍列表
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        总共有 {books.length} 本书
+      </Typography>
+      {books.map((book, index) => {
+        const imageUrl = `${imageHost}/${book.pic}`;
+        return (
+          <Card key={book._id} className={classes.card}>
+            <CardActionArea className={classes.actionArea}>
+              <CardMedia className={classes.cardMedia} image={imageUrl} title={book.name} />
+              <div className={classes.cardContent}>
+                <Typography variant="h5" component="h2">
+                  {book.title}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  图片：{book.pic}
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  编号：{index + 1}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  作者：{book.authors}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  标签：{book.tags}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  专题：{book.topics}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  阅读进度：{book.readingProgress}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  出版信息：{book.publisherInfo}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  isbn：{book.isbn}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  书评：{book.reviews}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  书摘：{book.excerpts}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  购买日期：{book.purchaseYear}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  出版日期：{book.publicationYear}
+                </Typography>
+              </div>
+            </CardActionArea>
+            <CardContent>
+              <IconButton aria-label="删除" color="secondary" onClick={() => handleDelete(book.name)}>
+                <DeleteIcon />
+              </IconButton>
+              <IconButton aria-label="编辑" color="primary" onClick={() => handleEdit(book.name)}>
+                <EditIcon />
+              </IconButton>
+            </CardContent>
+          </Card>
+        );
+      })}
     </div>
   );
 };
